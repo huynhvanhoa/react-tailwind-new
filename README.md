aabf1c7c164b38dbc375eb65ac5ccdb7e1f5b0f1
huynh van hoa <huynhvanhoa17011968@gmail.com>
2023-04-13 09:51:21 +0700

    react-tailwind-new

---
+++ README.md
@@ -0,0 +1,1 @@
+git remote add origin https://github.com/huynhvanhoa/react-tailwind-new.git
---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-3xl font-bold ">React tailwind new</h1>;
+}
+
+export default App;
